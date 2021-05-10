93c57b4c7e2cd04ea5ef7de8fe34673b384ce46c
vas777 <vas77794@gmail.com>
2021-05-10 10:23:05 +0200

    Add log level to build script

--- build-image.sh
+++ build-image.sh
@@ -39,7 +39,7 @@ fi
 #export DOCKER_CONTENT_TRUST=0
 
 # not that SCONE_STACK and SCONE_HEAPmust show the same values as define in application.sh - otherwise, MrEnclave will be different!
-MRENCLAVE=$(docker run -t --rm --entrypoint "" -e SCONE_ALPINE=1 -e SCONE_STACK=4M -e SCONE_HEAP=40M -e SCONE_HASH=1 registry.scontain.com:5050/sconecuratedimages/dapps:copy_demo /application)
+MRENCLAVE=$(docker run -t --rm --entrypoint "" -e SCONE_LOG=error -e SCONE_ALPINE=1 -e SCONE_STACK=4M -e SCONE_HEAP=40M -e SCONE_HASH=1 registry.scontain.com:5050/sconecuratedimages/dapps:copy_demo /application)
 
 verbose "MRENCLAVE=${MRENCLAVE}"
 
